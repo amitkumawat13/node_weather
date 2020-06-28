8b6c5734e4fa33ccc0da7dfd7da0d340696fcc74
amitkumawat13 <kumawatamit31@gmail.com>
2020-06-29 00:40:57 +0530

    testing git command

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const pubFolder = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
 const partialPath = path.join(__dirname, "../templates/partials");
 
-//setup handlebar engine and view location
+//setup handlebar engine and view location.
 app.set("view engine", "hbs");
 app.set("views", viewPath);
 hbs.registerPartials(partialPath);
